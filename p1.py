87f56d135a02b75b3ed8d14d5e191c3f2ce9daae
y0ngma <takeit2sy@pusan.ac.kr>
2019-12-16 17:00:11 +0900

    1일차 수업내용

---
+++ p1.py
@@ -0,0 +1,21 @@
+# 목록확인 :
+# $ dir ($은 powershell 에서 하는것임)
+# $ ls 
+# 디렉토리 이동
+# $ cd basic
+# 파이선 버전 확인 및 path 확인
+# $ python -V
+# => python 3.7.4
+# 파이선 구동 명령
+# python p1.py
+# File에서 Autosave 하거나 단축키 ctrl S 해서 터미널에서 실행 또는 우클릭에서
+# Run python file in terminal 실행.
+# F5에서 python 선택(디버깅모드)
+# or 주피터 노트북으로 진행
+
+print('hello world')
+
+a=1
+b=2
+a+b, a-b, a*b, a/b, 7%4, 7//4, 7/4 #나머지, 몫, 나눈값
+print(a+b, a-b, a*b, a/b, 7%4, 7//4, 7/4)
