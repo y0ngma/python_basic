6bdc030a28093df4527a157f43afa37e83b55b4a
y0ngma <takeit2sy@pusan.ac.kr>
2019-12-27 16:03:08 +0900

    day 9

--- python_web/flask01.py
+++ python_web/flask01.py
@@ -7,35 +7,51 @@
 # 127.0.0.1/join => naver.com/join
 
 from flask import Flask, render_template, request, redirect
+import cx_Oracle as oci  #
+    # pip install cx_oracle
+    # (base)C:\Users\admin ```$pip install cx_oracle```
+conn = oci.connect('admin/1234@192.168.99.100:32764/xe', encoding='utf-8') #
+    # 아이디/암호@서버주소:포트번호/SID
+print(conn)
+cursor = conn.cursor()
 
 app = Flask(__name__)
-
 @app.route("/", methods=['GET']) 
 def index():
+    print('index')
     return render_template('index.html') 
-
 @app.route("/join", methods=['GET']) 
 def join():
+    print('join-get')
     #return "join page <hr/><hr     />*5" # 줄넣어 꾸미기
     return render_template('join.html') 
     # 코드여러줄을 html화면으로 렌더링하라
-
 @app.route("/join", methods=['POST'])
 def join_post():
-    # DB에 값을 넣고
-    print("join-post")
-    return redirect("/login")  #
+    a = request.form['id']
+    b = request.form['pw']
+    c = request.form['name']
+    d = request.form['age']
+    sql = 'INSERT INTO MEMBER VALUES(:id, :pw, :na, :ag, SYSDATE)'
+    cursor.execute(sql, id=a, pw=b, na=c, ag=d)
+    conn.commit()
+    print('{}:{}:{}:{}'.format(a,b,c,d))
+    return redirect("/")  #
         # 127.0.0.1:5000/을 크롬에서 엔터친것 처럼 동작 
         # "/congrat" 가입축하페이지 등으로 가도 됨
-
+    # 오라클 DB접속하는법
+    # 추가하는 SQL문 작성 => INSERT, SELECT, UPDATE, DELETE
+    # SQL문 수행
 @app.route("/login", methods=['GET']) 
 def login():
+    print('login-get')
     return render_template('login.html') 
     
 @app.route("/login", methods=['POST']) 
 def login_post():
     print("login-post")
     return redirect("/")
+
 
 if __name__=='__main__':
     app.run(debug=True) # 
