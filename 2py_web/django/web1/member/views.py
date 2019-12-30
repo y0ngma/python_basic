7235fb50f1f8fa72c6178103aec4f74618b75ceb
y0ngma <takeit2sy@pusan.ac.kr>
2019-12-30 10:38:56 +0900

    123

--- 2py_web/django/web1/member/views.py
+++ 2py_web/django/web1/member/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'member/index')
+    return render(request, 'member/index.html')
     #return HttpResponse('index page <hr />') 처럼 하던 불편사항 개선
 def login(request):
     return ''
