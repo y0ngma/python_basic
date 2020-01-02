84d700221f7a082f5b06b71d317623e59385e0e0
y0ngma <takeit2sy@pusan.ac.kr>
2020-01-02 10:24:52 +0900

    model

--- 2py_web/django/web1/board/models.py
+++ 2py_web/django/web1/board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Table1(models.Model):
+    object = models.Manager() # vs code 오류 제거용
+
+    no      = models.AutoField(primary_key=True) # 자동 번호매기기
+        # 자동의 이점 : 개발자가 번호 중복 매기지 않을 수 있음
+# 사용자로부터 받을 내용-----------------------------
+    title   = models.CharField(max_length=200) 
+    content = models.TextField() 
+    writer  = models.CharField(max_length=50)
+# -------------------------------------------------
+    hit     = models.IntegerField() # 조회수
+    regdate = models.DateTimeField(auto_now_add=True) # 자동
