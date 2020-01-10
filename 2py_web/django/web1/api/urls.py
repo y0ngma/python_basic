566e75400c196949118bdd7e2648cb581564ceb3
y0ngma <takeit2sy@pusan.ac.kr>
2020-01-10 11:49:19 +0900

    serializers.

--- 2py_web/django/web1/api/urls.py
+++ 2py_web/django/web1/api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('insert1',       views.insert1,        name='insert1'),
     path('select1',       views.select1,        name='select1'),
+    path('select2',       views.select2,        name='select2'),
 
 
 
--- 2py_web/django/web1/api/views.py
+++ 2py_web/django/web1/api/views.py
@@ -1,4 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# insert1
+from .models import Item
+# select1
+from .serializers import ItemSerializer
+from rest_framework.renderers import JSONRenderer
+import json
+
+def insert1(request):
+    for i in range(1,31,1): 
+        # bulk_create 사용해서 중도중단시 아예 안올라가게 하자
+        obj = Item();
+        obj.name = '연필'+str(i)
+        obj.price= 1000+i
+        obj.save()
+    return HttpResponse('insert1')
+
+# {'id':'a'} 물품1개
 
 def select1(request):
+    key = request.GET.get('key', '')
+    data = json.dumps({'ret':'key error'})
+
+    if key == 'abc':
+        obj = Item.objects.get(no=1)
+        serializer = ItemSerializer(obj)
+        data = JSONRenderer().render(serializer.data)
+
+    return HttpResponse(data)
+
+# [{'id':'a', 'id':'b'}] 물품2개
+def select2(request):
+    obj = Item.objects.all()
+    serializer = ItemSerializer(obj, many=True)
+    data = JSONRenderer().render(serializer.data)
+    return HttpResponse(data)
+
+def select3(request):
     pass
