3a843b28f9b79c0fce784e38c33d2324a2059c9e
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-10 14:14:32 +0900

    의식흐름 3
    언어감지 누르면 통신성공

--- 5Flask/service/start.py
+++ 5Flask/service/start.py
@@ -1,6 +1,6 @@
 # flask 기본 구성
 # 1. 모듈가져오기
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # 2. 플라스크 객체 생성
 app = Flask(__name__)
 # 3. 라우팅
@@ -9,9 +9,12 @@ def home():
     return 'hello flask'
 # 3-1. 언어감지 처리
 # GET반식만 현재 되어 있는데, POST도 지원하겠다
-@app.route('/LangTypeDetect')
+@app.route('/LangTypeDetect', methods=['GET', 'POST'])
 def LangTypeDetect():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return { 'code':1, 'lang':'en', 'desc':'영어' }
+    if request.method == 'GET': # else:
+        return render_template('index.html')
 
 # 4. 서버가동
 if __name__=='__main__':
