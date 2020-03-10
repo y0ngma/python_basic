9f9f22c5f6f94adee31924227557266bc6e38cf3
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-10 14:18:41 +0900

    ImmutableMultiDict

--- 5Flask/service/start.py
+++ 5Flask/service/start.py
@@ -9,9 +9,15 @@ def home():
     return 'hello flask'
 # 3-1. 언어감지 처리
 # GET반식만 현재 되어 있는데, POST도 지원하겠다
+# 1개의 url로 여러 메소드를 지원 => restful
 @app.route('/LangTypeDetect', methods=['GET', 'POST'])
 def LangTypeDetect():
     if request.method == 'POST':
+        # 1. 원문데이터 획득(get, post 방식으로 전달된 데이터 획득)
+        print( request.form )
+        # 2. 언어 감지
+        # 2-1. DB에 로그처리
+        # 3. 응답
         return { 'code':1, 'lang':'en', 'desc':'영어' }
     if request.method == 'GET': # else:
         return render_template('index.html')
