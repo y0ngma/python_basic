b6eb05147024ee47f349cb417e20f4c186455f0c
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-10 14:06:52 +0900

    의식흐름3-1
    405(METHOD NOT ALLOWED)

--- 5Flask/service/start.py
+++ 5Flask/service/start.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 # 3. 라우팅
 @app.route('/') # 데코레이터. 함수안의 함수
 def home():
+    return 'hello flask'
+# 3-1. 언어감지 처리
+# GET반식만 현재 되어 있는데, POST도 지원하겠다
+@app.route('/LangTypeDetect')
+def LangTypeDetect():
     return render_template('index.html')
+
 # 4. 서버가동
 if __name__=='__main__':
     app.run(debug=True)
