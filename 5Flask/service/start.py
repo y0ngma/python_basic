674891b05af89558746ff8b96b478e1a5dbe5230
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-11 13:06:11 +0900

    자치구 제외된 지도

--- 5Flask/service/start.py
+++ 5Flask/service/start.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # 3. 라우팅
 @app.route('/') # 데코레이터. 함수안의 함수
 def home():
-    return render_template('test.html')
+    return render_template('dom.html')
 # 3-1. 언어감지 처리
 # GET반식만 현재 되어 있는데, POST도 지원하겠다
 # 1개의 url로 여러 메소드를 지원 => restful
