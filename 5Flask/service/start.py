6bad77d762a4b924ae8b73a1b0130beddaf244b9
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-11 13:34:31 +0900

    객체함수작성
    1. 내부에는 맴버변수, 맴버함수
    2. 함수def getAreaGps(): 추가

--- 5Flask/service/start.py
+++ 5Flask/service/start.py
@@ -13,6 +13,12 @@ app = Flask(__name__)
 @app.route('/') # 데코레이터. 함수안의 함수
 def home():
     return render_template('dom.html')
+
+@app.route('/getAreaGps') # 
+def getAreaGps():
+    return [ {'lat':37.55487682, 'lng':126.9696652},
+            {'lat':37.55487682, 'lng':126.9696652} ]
+
 # 3-1. 언어감지 처리
 # GET반식만 현재 되어 있는데, POST도 지원하겠다
 # 1개의 url로 여러 메소드를 지원 => restful
