87d7ae92858525b39ffb11bd96986dc08a4bf60c
y0ngma <takeit2sy@pusan.ac.kr>
2020-03-19 10:17:25 +0900

    2

--- Functions/01data.py
+++ Functions/01data.py
@@ -1,3 +1,12 @@
 def loadData( filename, dir='/content/drive/My Drive/data/ml-100k' ):
-    pass
+    data = list()
+    array = list()
+    # 파일을 읽어서(한줄씩) => 바로 원하는 형태로 처리
+    with open( f'{dir}/{filename}' ) as f:
+        # 한줄씩 읽는다
+        for line in f:
+            pass
+        pass
+    return ( data, np.array(array) )
+
 loadData( 'ua.base' ) # ua.test
